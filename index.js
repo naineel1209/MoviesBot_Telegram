8afb22227231c18fe27ba7a39dd56313a37c606e
Naineel <naineelsoyantar@gmail.com>
2023-03-12 15:02:15 +0530

    Second Commit

--- index.js
+++ index.js
@@ -133,22 +133,12 @@ bot.on('callback_query', async (ctx) => {
 
 
 // Start the server
-if (process.env.NODE_ENV === "production") {
-    // Use Webhooks for the production server
-    const app = express();
-    app.use(express.json());
-    app.use(webhookCallback(bot, "express"));
-
-    const PORT = process.env.PORT || 3000;
-    app.listen(PORT, () => {
-        console.log(`Bot listening on port ${PORT}`);
-    });
-} else {
+{
     // Use Long Polling for development
-    bot.start();
+    // bot.start();
+    bot.launch();
 }
 
-bot.launch();
 
 process.once("SIGINT", () => { bot.stop("SIGINT"); })
 process.once("SIGTERM", () => { bot.stop("SIGTERM"); })
